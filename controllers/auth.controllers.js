9aea2c2d013f010bec66832ef24a76caf534213c
Your Name <you@example.com>
2024-09-07 23:19:54 +0100

    auth

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,4 +1,5 @@
-const User = require("../model/user.model")
+const User = require("../model/user.model");
+const JWT = require("jsonwebtoken")
 
 
 async function signupUser(req, res) {
@@ -28,30 +29,23 @@ async function signupUser(req, res) {
 async function loginUser(req, res) {
     try {
         let userExists = await User.findOne({email: req.body.email});
-        // if(userExists) {
-        //     const passwordMatch = userExists.checkPassword(req.body.password);
-        //     if (passwordMatch) {
-        //         let token = userExists.generateToken();
-        //         res.status(201).json({
-        //             message: "login successful",
-        //             userExists,
-        //             token,
-        //         })
-            // } else {
-            //     res.status(400).json({ message: "incorect password" })
-            // }
-            if (userExists.password === req.body.password) {
-            res.status(201).json({
-                message: "login successful", userExists
-            });
+        if(userExists) {
+            const passwordMatch = userExists.checkPassword(req.body.password);
+            if (passwordMatch) {
+                let token = userExists.generateToken();
+                res.status(201).json({
+                    message: "login successful",
+                    userExists,
+                    token,
+                })
             } else {
-                res.status(400).json({message: "incorect password"})
+                res.status(400).json({ message: "incorect password" })
             }
-        // } else {
-        //     res.status(404).json({
-        //         message: "user does not exist, please sign up",
-        //     });
-        // }
+        } else {
+            res.status(404).json({
+                message: "user does not exist, please sign up",
+            });
+        }
     } catch (error) {
         console.log(error)
         res.status(500).json({
@@ -60,5 +54,22 @@ async function loginUser(req, res) {
     }
 }
 
+const isTokenValid = (req, res, next) => {
+    try {
+        const longToken = req.headers.authorization;
+        if(longToken) {
+            return res.status(400).json({message: "token not present"});
+        }; 
+        const token = longToken.split("")[1];
+        let user = JWT.verify(token, process.env.JWT_SECRET);
+        res.user = user;
+        next();
+    } catch (error) {
+        console.log(error);
+        return res.status(401).json({message: "invalid token"});
+    }
+};
 
-module.exports = { signupUser, loginUser };
+
+
+module.exports = { signupUser, loginUser, isTokenValid };
